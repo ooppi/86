af9d5494044892412498896eca0e28ce8eca282b
ooppi <49905278+ooppi@users.noreply.github.com>
2024-11-02 23:13:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default                                    #kenzok8软件库
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default                                               #kenzok8软件库依赖
-echo 'src-git ImmortalWrt https://github.com/immortalwrt/luci' >>feeds.conf.default
+# echo 'src-git ImmortalWrt https://github.com/immortalwrt/luci' >>feeds.conf.default
