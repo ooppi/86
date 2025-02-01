20c08a0316035f3feed2056cc052fbfff413f4a5
ooppi <49905278+ooppi@users.noreply.github.com>
2025-02-01 16:25:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.30/g' package/base-files/luci2/bin/config_generate 
 
 # Modify hostname
-sed -i 's/OpenWrt/Kurty-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Kurty-Router/g' package/base-files/luci2/bin/config_generate 
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings                                              # 设置密码为空
